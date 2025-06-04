1893e1d2ca66485b61ead543ce7c753de3675b48
Danni Garcia <jodadev.col@gmail.com>
2025-06-04 21:27:59 +1000

    feat: Add navigation data structure for site links

---
+++ src/data/navigation.js
@@ -0,0 +1,19 @@
+const Navigation = [
+  {
+    id: 'home_link',
+    label: 'Home',
+    link: '/'
+  },
+  {
+    id: 'about_link',
+    label: 'About',
+    link: '/about'
+  },
+  {
+    id: 'create-plan_link',
+    label: 'Create Your Plan',
+    link: '/create-your-plan'
+  },
+]
+
+export default Navigation;
